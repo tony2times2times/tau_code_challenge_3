98ea8c9196badb011934e20d6a57150362d72617
Millie Walsh <millie@primeacademy.io>
2016-09-08 17:30:21 -0500

    first pass on node code challenge

---
+++ server/app.js
@@ -0,0 +1,18 @@
+// initial jokes provided by the client
+jokes = [
+  {
+    whoseJoke: "Huck",
+    jokeQuestion: "What's the difference between roast beef and pea soup?",
+    punchLine: "Anyone can roast beef."
+  },
+  {
+    whoseJoke: "Millie",
+    jokeQuestion: "What do you call a pile of cats?",
+    punchLine: "A mewontain!"
+  },
+  {
+    whoseJoke: "dEv",
+    jokeQuestion: "Why should you not play cards the forest?",
+    punchLine: "Too many Cheetahs"
+  }
+];
