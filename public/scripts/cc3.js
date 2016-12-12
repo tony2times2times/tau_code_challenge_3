0986e2cd618a8f45e5831cfa8425346597fe843b
devjanaprime <dev@primeacademy.io>
2016-12-12 10:50:10 -0600

    added a Luke joke and both uses of urlencoded

--- public/scripts/cc3.js
+++ public/scripts/cc3.js
@@ -2,5 +2,17 @@ console.log( 'js' );
 
 $( document ).ready( function(){
   console.log( 'JQ' );
-  
+  $( '#addJokeButton' ).on( 'click', function(){
+    var objectToSend = {
+      asdf: 'qwer'
+    }; // end objectToSend
+    $.ajax({
+      url: '/testPost',
+      type: 'POST',
+      data: objectToSend,
+      success: function( response ){
+        console.log( 'back from ajax:', response );
+      } //end success  
+    });// end ajax
+  });
 });
--- server/app.js
+++ server/app.js
@@ -2,14 +2,21 @@ var express = require( 'express' );
 var app = express();
 var path = require( 'path' );
 var bodyParser = require( 'body-parser' );
+// create 'urlEncodedParser' in case we want to inject it for post calls:
 var urlEncodedParser = bodyParser.urlencoded( { extended: true } );
-
+// use bodyParser.urlencoded throughout the app with this:
+app.use( bodyParser.urlencoded( { extended: false } ) );
 // initial jokes provided by the client
 jokes = [
   {
     whoseJoke: "Danny",
     jokeQuestion: "Why do scuba divers fall backwards out of boats?",
     punchLine: "If they fell forwards they’d still be in the boat"
+  },
+  {
+    whoseJoke: "Luke",
+    jokeQuestion: "Twofish are in a tank. What did one fish say to the other?",
+    punchLine: "Do you know how to drive this thing?"
   },
   {
     whoseJoke: "Millie",
@@ -32,6 +39,6 @@ app.get( '/', function( req, res ){
   // base url
   console.log( 'base url hit' );
   res.sendFile( path.resolve( 'views/index.html' ) );
-});
+}); // end base url
 
 app.use( express.static( 'public' ) );
